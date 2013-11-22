80ce07e06a2b24306a5a423a5f37cfc36a7c351e
Andrew Magliozzi <andrew.magliozzi@gmail.com>
2013-11-22 11:17:22 -0500

    refactoring hoverbox and rangeslider into one plugin

--- src/rangeslider.js
+++ src/rangeslider.js
@@ -421,6 +421,143 @@ videojs.ControlBar.prototype.options_.children.ControlTimePanel={}; //Panel with
 
 
 
+/**
+ * This is the hover box
+ * @param {videojs.Player|Object} player
+ * @param {Object=} options
+ * @constructor
+ */
+videojs.HoverBox = videojs.Component.extend({
+  /** @constructor */
+  init: function(player, options){
+    videojs.Component.call(this, player, options);
+  }
+});
+
+// -- Helper variables and function for mouseover box countdown
+
+// TODO: Refactor these static values as plugin Options
+
+// TODO : what is this? (add code review message in github)
+videojs.HoverBox.prototype.timer = 1.5;
+
+// This is a static variable used to reset the timer 
+videojs.HoverBox.prototype.duration = 1.5;
+
+// This value is used to start a highlight prior to the completion of the animation  
+videojs.HoverBox.prototype.buffer = 5;
+
+// countdown interval
+videojs.HoverBox.prototype.countDownInterval = 500;
+
+// this is a variable to toggle on and off on mouseenter
+videojs.HoverBox.prototype.set = false;
+
+// this is a variable to store the setInterval value which will get cleared onmouseleave
+videojs.HoverBox.prototype.interval = undefined;
+
+// this is a variable to store the setTimeout value which will get cleared onmouseleave
+videojs.HoverBox.prototype.timeout = undefined;
+
+videojs.HoverBox.prototype.link = undefined;
+
+// this is a method to adjust the highlight hover animation in the plugin
+// TODO: Clean up this function.
+videojs.HoverBox.prototype.countDown = function () {
+  var el = this.element;
+
+  elCounter = this.elementCounter;
+
+  if (this.timer > 0) { // if the timer is greater than zero, addClass 'counting' and decrement timer
+    $(el)
+      .removeClass('active')
+      .addClass('counting');
+    $(elCounter).html("Highlighting");
+    this.timer -= this.countDownInterval/1000;
+  }
+  else if (this.timer === 0) { // if the timer equals zero, change the innerHTML and decrement timer
+    $(elCounter).html('Don\'t move!');
+    this.timer -= this.countDownInterval/1000;
+    // TODO: remove the final else block and adjust the animation so it ends at timer === 0...
+  } else { // stop the timer by clearing the interval
+    clearInterval(this.interval);
+    $(el).addClass('counting-done');
+    $(elCounter).html('Clipping!');
+  }
+};
+
+videojs.HoverBox.prototype.endClip = function(){
+    $(this.element).removeClass('counting');
+    clearInterval(this.interval);
+    clearTimeout(this.timeout);
+    this.set = false;
+
+  if ( $(event.srcElement).hasClass('counting-done') ) {
+    this.endTime = this.player_.currentTime();
+    this.set = false;
+    this.link = $(location).attr('href').toString() + '?start=' + this.startTime + '&end=' + this.endTime;
+		$("#share-link").val(this.link);
+  }
+
+  this.reset.call(this);
+};
+
+// Rest all hoverbox attributes
+videojs.HoverBox.prototype.reset = function(){
+  el = this.element;
+    $(el)
+      .removeClass('counting')
+    $(this.elementCounter).html("highlight");
+  this.timer = this.duration;
+  return this;
+};
+
+// Create the hoverbox DOM element
+videojs.HoverBox.prototype.createEl = function(){
+  // This is the main hoverbox element
+  this.element = videojs.Component.prototype.createEl.call(this, 'div', {
+    className: 'vjs-hoverbox'
+  });
+
+  // This is the counter element inside the main hoverbox element
+  this.elementCounter = videojs.Component.prototype.createEl.call(this, 'span', {
+    className: 'vjs-hoverbox-counter'
+  });
+
+  $(this.element).append(this.elementCounter);
+
+  // on mouse enter: start the countdown and init the timeout
+  this.element.onmouseenter = function() {
+    $(this.element).removeClass('counting-done');
+
+    // register the periodic countdown call and store the pointer
+    this.interval = setInterval(function() {
+      this.countDown.call(this, this.element);
+    }.bind(this), this.countDownInterval);
+
+    this.timeout = setTimeout(function() {
+      if (!this.set) {
+        this.set = true;
+        // logic to set highlight start time before the user took action
+        if (this.player_.currentTime() < this.buffer) {
+          this.startTime = 0;
+        } else {
+          this.startTime = this.player_.currentTime() - this.buffer;
+        }
+      }
+    }.bind(this), this.duration);
+  }.bind(this);
+
+  // on mouse leave: reset the animation, clear timeout and countdown 
+  this.element.onmouseleave = function(event) {
+    this.endClip.call(this);
+  }.bind(this);
+
+  return this.element;
+};
+
+
+
 //-- Design the new components
 
 /**
